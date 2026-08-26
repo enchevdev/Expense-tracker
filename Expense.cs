6d359e138831d232a16de1f7fbebec9ce302f522
enchevdev <enchev.dev@gmail.com>
2026-08-26 16:01:36 +0200

    Add Expense model

---
+++ Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker
+{
+    internal class Expense
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+
+
+
+    }
+}
