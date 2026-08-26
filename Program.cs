976e88e22e8458ab8ea422fe6af253ceff71c4fa
enchevdev <enchev.dev@gmail.com>
2026-08-26 16:34:52 +0200

    Display multiple expenses

--- Program.cs
+++ Program.cs
@@ -8,17 +8,37 @@
 
 
             Expense expense = new Expense();
+            Expense expense1 = new Expense();
+            Expense expense2 = new Expense();
+
 
             List<Expense> expenses = new List<Expense>();
+
+
+
 
             expense.Description = "Pizza";
             expense.Amount = 15.00m;
             expense.Category = "Food";
 
+            expense1.Description = "Cola";
+            expense1.Amount = 3.00m;
+            expense1.Category = "Food";
+
+            
+            expense2.Description = "Train ticket";
+            expense2.Amount = 12.00m;
+            expense2.Category = "Transportation";
+
             expenses.Add(expense);
+            expenses.Add(expense1);
+            expenses.Add(expense2);
 
-            Console.WriteLine(expense.Description + " - $" + expense.Amount + " - " + expense.Category);
 
+            foreach ( Expense item in expenses)
+            {
+                Console.WriteLine($"Description: {item.Description}, Amount: {item.Amount}, Category: {item.Category}");
+            }
         }
     }
 }
